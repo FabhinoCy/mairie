dda42851fc4c4e3eaa04cb104e7e056a68468ca2
Fabien CURFS <fabien.curfs@etudiant.univ-reims.fr>
2022-12-20 20:47:52 +0100

    suite

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-
-
     #[Route('/', name: 'app_evenement_calendar', methods: ['GET'])]
-    public function calendar(): Response
+    public function index(): Response
     {
         return $this->render('evenement/calendar.html.twig');
+    }
+
+    #[Route('/calendrier', name: 'app_calendrier')]
+    public function calendrier(): Response
+    {
+        return $this->render('calendrier.html.twig');
+    }
+
+    #[Route('/blog', name: 'app_blog')]
+    public function blog(): Response
+    {
+        return $this->render('calendrier.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('calendrier.html.twig');
     }
 }
