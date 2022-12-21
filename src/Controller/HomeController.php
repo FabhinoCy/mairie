1921316db402e6fa2a376ab139ecd1aaa8383cd2
Fabien CURFS <fabien.curfs@etudiant.univ-reims.fr>
2022-12-21 21:38:06 +0100

    suite

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Evenement;
+use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_evenement_calendar', methods: ['GET'])]
-    public function index(): Response
+    public function index(EvenementRepository $evenementRepository): Response
     {
-        return $this->render('evenement/calendar.html.twig');
+        return $this->render('evenement/calendar.html.twig', [
+            'events' => $evenementRepository->findAll(),
+        ]);
     }
 
     #[Route('/calendrier', name: 'app_calendrier')]
