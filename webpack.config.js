927075ac4ae405f5fcabc5a37c28653bcf236199
Liam Boudraa <liam.boudraa@etudiant.univ-reims.fr>
2022-12-31 12:01:06 +0100

    Fix - navigation style

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,8 @@ Encore
      */
     .addEntry('app', './assets/app.js')
 
+    .addStyleEntry('css/member', './assets/styles/member.scss')
+
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
 
@@ -70,7 +72,7 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+    .autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
